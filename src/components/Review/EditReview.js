d35e25aa88622130bea365d06066071eab824ef1
sookim-Boston <soojeong914@gmail.com>
2019-10-16 21:41:20 -0400

    Add correct route
    - Add a dummy route for edit

--- src/components/Review/EditReview.js
+++ src/components/Review/EditReview.js
@@ -8,9 +8,10 @@ import ReviewForm from './ReviewForm'
 const EditReview = ({ user, match, alert, history }) => {
   const reviewObject = {
     rating: '',
-    url: '',
-    fileType: ''
+    url: ''
   }
+  const [review, setReview] = useState(reviewObject)
+  // const [update, setUpdate] = useState(false)
 
   useEffect(() => {
     axios({
@@ -20,31 +21,42 @@ const EditReview = ({ user, match, alert, history }) => {
         'Authorization': `Bearer ${user.token}`
       }
     })
-      .then(responseData => setreview(responseData.data.review))
+      // .then(responseData => {
+      //   setReview(responseData.data.review)
+      // })
       .catch(console.error)
   }, [])
 
-  const [review, setreview] = useState(reviewObject)
   const handleChange = event => {
     event.persist()
-    setreview(() => ({ ...review, [event.target.name]: event.target.value })
+    setReview(() => ({ ...review, [event.target.name]: event.target.value })
     )
   }
 
   const handleSubmit = event => {
     event.preventDefault()
+    const formData = new FormData(event.target)
     axios({
       url: `${apiUrl}/items/${match.params.id}/reviews/${match.params.rid}`,
       method: 'PATCH',
-      data: { review },
+      data: formData,
       headers: {
         'Authorization': `Bearer ${user.token}`
       }
     })
       .then(() => alert({ heading: 'Success', message: 'You updated a review', variant: 'success' }))
-      .then(() => history.push(`/items/${match.params.id}`))
+      // .then(() => setUpdate(true))
+      .then(() => history.replace('/reload'))
+    // history.replace('/reload')
+    // history.replace(`/items/${match.params.id}`)
+
+      .then(() => history.replace(`/items/${match.params.id}`))
       .catch(() => alert({ heading: 'Rut roh', message: 'Something went wrong', variant: 'danger' }))
   }
+
+  // if (update) {
+  //   return <Redirect to={`/items/${match.params.id}`}/>
+  // }
 
   return (
     <ReviewForm
