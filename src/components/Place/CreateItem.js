3bf19f24c4e1658be047dc1dc75d870bae539750
sookim-Boston <soojeong914@gmail.com>
2019-10-25 21:30:04 -0400

    Add component
    -Created a route to create an item

---
+++ src/components/Place/CreateItem.js
@@ -0,0 +1,52 @@
+import React, { useState, Fragment } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import { withRouter } from 'react-router-dom'
+import ItemForm from './ItemForm'
+
+const style = {
+  fontFamily: 'Maven Pro',
+  fontSize: '20px',
+  textAlign: 'left',
+  padding: '15px'
+}
+
+const CreateItem = ({ user, match, alert, history }) => {
+  const [item, setItem] = useState({
+    name: ''
+  })
+
+  const handleChange = event => {
+    event.persist()
+    setItem(() => ({ ...item, [event.target.name]: event.target.value }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    axios({
+      url: `${apiUrl}/places/${match.params.id}/items`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      },
+      data: { item }
+    })
+      .then(response => response.data.item._id)
+      .then((id) => {
+        history.replace('/reload')
+        history.replace('/items/' + id + '/reviews')
+      })
+  }
+  return (
+    <Fragment>
+      <h1 style={style}>What dish did you have?</h1>
+      <ItemForm
+        item ={item}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    </Fragment>
+  )
+}
+
+export default withRouter(CreateItem)
