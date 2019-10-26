9f44dacf714d9843e51c2aea1ac447628b117851
sookim-Boston <soojeong914@gmail.com>
2019-10-25 21:37:30 -0400

    Remove a Form
    -Removed a form to create new item
    -Created CreateItem instead

--- src/components/Place/Place.js
+++ src/components/Place/Place.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import { withRouter, Link } from 'react-router-dom'
-// import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
+import { Card, CardTitle, CardText, Row, Col } from 'reactstrap'
 
-const Place = ({ user, alerts, match }) => {
+const style = {
+  // boxShadow: '1px -1px 1px 1px rgba(0,0,0,0.75)',
+  height: '18vh',
+  margin: '6px',
+  fontFamily: 'Maven Pro',
+  fontSize: '15px'
+}
+
+const Place = ({ user, alert, match, history }) => {
   const [place, setplace] = useState({ items: [], name: '' })
   // const [deleted, setDeleted] = useState(false)
-
   // dependency list -> we can run dependency list and anytime it changes, I would like you to run this function. Use this effect and treated as componentdidmount
   useEffect(() => {
     axios({
@@ -25,15 +33,22 @@ const Place = ({ user, alerts, match }) => {
   }
 
   const itemJsx = place.items.map(item => (
-    <p key={item._id}>
-      <Link to={`/items/${item._id}`}>{item.name}</Link>
-    </p>
+    <Col md="3" sm="6" key={item._id}>
+      <Card body style={style}>
+        <CardTitle><Link to={`/items/${item._id}`}>{item.name}</Link></CardTitle>
+        <CardText></CardText>
+      </Card>
+    </Col>
   ))
 
   return (
     <Fragment>
-      <h1>{place.name}</h1>
-      {itemJsx}
+      <h1 style={{ textAlign: 'center', fontFamily: 'Cabin Condensed', fontSize: '25px', margin: '10px' }}>{place.name}</h1>
+      <p style={{ textAlign: 'center', fontFamily: 'Cabin Condensed', fontSize: '20px' }}>
+        {'Can\'t find your dish? Be the first to review!'} <br/>
+        <Button style={{ backgroundColor: 'white', borderColor: 'white' }}><Link to={`/places/${match.params.id}/items`}> Write a review</Link></Button>
+      </p>
+      <Row>{itemJsx}</Row>
     </Fragment>
   )
 }
